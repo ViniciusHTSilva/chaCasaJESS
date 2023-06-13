92b63639009198c356a40ac22ad76f9751dcb79d
Vinicius <88394501+ViniciusHTSilva@users.noreply.github.com>
2023-06-12 23:39:36 -0300

    update

--- components/Init.jsx
+++ components/Init.jsx
@@ -14,7 +14,9 @@ export default function Init() {
             <p>Quero compartilhar com vocês a alegria de ter meu próprio cantinho!</p>
             </div>
 
-       
+            <div className="jess">
+                <img src="https://i.ibb.co/s9BFyYV/jess.png" alt="" />
+            </div>
 
            
           
