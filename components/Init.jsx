b7459f62010cf05681b02b9c6cc32f2dd8b96ce3
Vinicius <88394501+ViniciusHTSilva@users.noreply.github.com>
2023-06-14 00:03:10 -0300

    update

--- components/Init.jsx
+++ components/Init.jsx
@@ -18,6 +18,27 @@ export default function Init() {
                 <img src="https://i.ibb.co/ryZxVs3/Screenshot-20230612-234125-Whats-App-fotor-bg-remover-20230612234217.png" alt="" />
             </div>
 
+            <div class="container">
+        
+            <div class="data">
+                <h1>Data</h1>
+            <p>08/07</p>
+            </div>
+         
+            
+            <div class="horario">
+                <h1>Horario</h1>
+                <p>16:00</p>
+            </div>
+        
+
+            <div class="endereco">
+                <h1>Endereço</h1>
+                <p>Rua Professor Arnaldo Joao Semeraro, 580 - Jd Sta Emilia(Salão de Festas)</p>
+            </div>
+        </div>
+    
+     
            
           
 
